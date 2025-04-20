314cf5d4b94ed4c75f82ed4e9ddfa327a2fe4c29
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-04-20 12:37:52 +0300

    Hack up the project and show the products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import { useState, useEffect } from "react";
+
+interface ProductProps {
+  pid: string;
+  name: string;
+  description: string;
+  price: number;
+  images: string;
+}
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://api.jotform.com/form/251073431687963/payment-info?apiKey=e3b866e087d1eaf6e1e450c94fc55de1",
+      );
+      const data = await response.json();
+
+      setProducts(data.content.products);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <div>hey</div>
+      <h1 className="text-2xl font-bold">Products</h1>
+      <div>
+        {products.map((product: ProductProps) => (
+          <li key={product.pid}>
+            <h2>{product.name}</h2>
+            <img
+              src={JSON.parse(product.images)}
+              alt={product.name}
+              className="h-32 w-32"
+            />
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <button className="rounded bg-blue-500 px-4 py-2 text-white">
+              Add to Cart
+            </button>
+          </li>
+        ))}
+      </div>
     </>
   );
 }
